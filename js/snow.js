98068c7dd7f5cb3b6eec4d0975c05263f6b85618
shaoshao <1600767556@qq.com>
2021-09-21 12:28:20 +0800

    Site updated: 2021-09-21 12:28:12

--- js/snow.js
+++ js/snow.js
@@ -1,5 +1,3 @@
-<script type="text/javascript" src="http://libs.baidu.com/jquery/1.8.3/jquery.js"></script>
-<script type="text/javascript" src="http://libs.baidu.com/jquery/1.8.3/jquery.min.js"></script>
 
 /*样式一*/
 (function($){
