98b870402d33f50f20c584f3ad3dae16b2d06c9d
shaoshao <1600767556@qq.com>
2021-09-22 13:12:55 +0800

    Site updated: 2021-09-22 13:12:35

--- js/matery.js
+++ js/matery.js
@@ -170,3 +170,23 @@ $(function () {
     // 初始化加载 tooltipped.
     $('.tooltipped').tooltip();
 });
+
+
+
+
+
+    // 深色模式设置
+    function switchNightMode() {
+        var body = document.body;
+        if(body.classList.contains('dark')){
+        document.body.classList.remove('dark');
+        localStorage.setItem('dark','0');
+        $('#nightMode').removeClass("fa-lightbulb-o").addClass("fa-moon-o");
+        return;
+        } else {
+        document.body.classList.add('dark');
+        localStorage.setItem('dark','1');
+        $('#nightMode').removeClass("fa-moon-o").addClass("fa-lightbulb-o");
+        return;
+        }
+    }
