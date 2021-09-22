ac9dcc9a1b9732644b7857718062504573380657
shaoshao <1600767556@qq.com>
2021-09-22 12:59:59 +0800

    Site updated: 2021-09-22 12:59:41

--- js/matery.js
+++ js/matery.js
@@ -170,3 +170,16 @@ $(function () {
     // 初始化加载 tooltipped.
     $('.tooltipped').tooltip();
 });
+
+
+//提醒开启功能
+    setTimeout(function () {
+      if ((new Date().getHours() >= 19 || new Date().getHours() < 7) && !$('body').hasClass('DarkMode')) {
+        let toastHTML = '<span style="color:#97b8b2;border-radius: 10px;>' + '<i class="fa fa-bell" aria-hidden="true"></i>晚上使用深色模式阅读更好哦。(ﾟ▽ﾟ)/</span>'
+        M.toast({ html: toastHTML })
+      }
+    }, 2200)
+//定时开启功能由于定时开启功能会导致强制深色模式，我太拉跨没办法解决，不推荐大家开启
+if (new Date().getHours() >= 22 || new Date().getHours() < 6) {
+        document.body.classList.add('DarkMode');
+         } 
