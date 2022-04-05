81c3e660942afc18732c6f62c93ddcac5e28748d
shaoshao <1600767556@qq.com>
2022-04-05 18:23:36 +0800

    Site updated: 2022-04-05 18:23:22

--- js/matery.js
+++ js/matery.js
@@ -11,7 +11,22 @@ $(function () {
         });
     };
     articleCardHover();
-
+// 网站全局深色模式设置开始
+function switchNightMode() {
+    var body = document.body;
+    if(body.classList.contains('dark')){
+      document.body.classList.remove('dark');
+      localStorage.setItem('dark','0');
+      $('#nightMode').removeClass("fa-lightbulb-o").addClass("fa-moon-o");
+      return;
+    } else {
+      document.body.classList.add('dark');
+      localStorage.setItem('dark','1');
+      $('#nightMode').removeClass("fa-moon-o").addClass("fa-lightbulb-o");
+      return;
+    }
+};
+// 网站全局深色模式设置结束
     /*菜单切换*/
     $('.sidenav').sidenav();
 
