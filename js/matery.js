64cc606fcafd743869e3909987662f6fbd0609fd
shaoshao <1600767556@qq.com>
2022-04-05 18:10:01 +0800

    Site updated: 2022-04-05 18:08:42

--- js/matery.js
+++ js/matery.js
@@ -15,22 +15,6 @@ $(function () {
     /*菜单切换*/
     $('.sidenav').sidenav();
 
-    // 深色模式设置
-    function switchNightMode() {
-        var body = document.body;
-        if(body.classList.contains('dark')){
-        document.body.classList.remove('dark');
-        localStorage.setItem('dark','0');
-        $('#nightMode').removeClass("fa-lightbulb-o").addClass("fa-moon-o");
-        return;
-        } else {
-        document.body.classList.add('dark');
-        localStorage.setItem('dark','1');
-        $('#nightMode').removeClass("fa-moon-o").addClass("fa-lightbulb-o");
-        return;
-        }
-    }
-
     /* 修复文章卡片 div 的宽度. */
     let fixPostCardWidth = function (srcId, targetId) {
         let srcDiv = $('#' + srcId);
