2498c824cf623f7ad00726b2206ad7c766b32a45
shaoshao <1600767556@qq.com>
2022-04-05 19:13:21 +0800

    Site updated: 2022-04-05 19:12:31

--- js/matery.js
+++ js/matery.js
@@ -12,20 +12,14 @@ $(function () {
     };
     articleCardHover();
 // 网站全局深色模式设置开始
-function switchNightMode() {
-    var body = document.body;
-    if(body.classList.contains('dark')){
-      document.body.classList.remove('dark');
-      localStorage.setItem('dark','0');
-      $('#nightMode').removeClass("fa-lightbulb-o").addClass("fa-moon-o");
-      return;
-    } else {
-      document.body.classList.add('dark');
-      localStorage.setItem('dark','1');
-      $('#nightMode').removeClass("fa-moon-o").addClass("fa-lightbulb-o");
-      return;
+//黑夜模式提醒开启功能
+setTimeout(function () {
+    if ((new Date().getHours() >= 19 || new Date().getHours() < 7) && !$('body').hasClass('DarkMode')) {
+        let toastHTML = '<span style="color:#97b8b2;border-radius: 10px;>' + '<i class="fa fa-bellaria-hidden="true"></i>晚上使用黑夜模式阅读能够减轻视觉疲劳。</span>'
+        M.toast({ html: toastHTML })
     }
-};
+}, 2000)
+
 // 网站全局深色模式设置结束
     /*菜单切换*/
     $('.sidenav').sidenav();
